634c5d5e7ca3434b4dbcc1d42d275f560815a54c
junmiao <mj287@163.com>
2023-02-23 09:03:28 +0800

    components

---
+++ components/index.js
@@ -0,0 +1,71 @@
+/**
+ * 组件
+ */
+import ColumnTitle from './column-title'
+import Data from './data'
+import Dialog from './dialog'
+import Dragger from './dragger'
+import Drawer from './drawer'
+import EditorCode from './editor-code'
+import Empty from './empty'
+import FieldDate from './field-date'
+import FieldTable from './field-table'
+import FieldText from './field-text'
+import FieldTree from './field-tree'
+import Img from './img'
+import InputNumber from './input-number'
+import ListMenu from './list-menu'
+import ListMenuItem from './list-menu-item'
+import PowerPage from './power-page'
+import Price from './price'
+import Render from './render'
+import Search from './search'
+import SearchItem from './search-item'
+import Select from './select'
+import Splitter from './splitter'
+import Table from './table'
+import TableColumnFixed from './table-column-fixed'
+import TablePagination from './table-pagination'
+import TableSplitter from './table-splitter'
+import TableSummary from './table-summary'
+import Thumbnail from './thumbnail'
+import Toolbar from './toolbar'
+import Uploader from './uploader'
+import UploaderQuery from './uploader-query'
+import ValueFormat from './value-format'
+
+export default function(Vue) {
+
+    Vue.component(ColumnTitle.name, ColumnTitle)
+    Vue.component(Data.name, Data)
+    Vue.component(Dialog.name, Dialog)
+    Vue.component(Dragger.name, Dragger)
+    Vue.component(Drawer.name, Drawer)
+    Vue.component(EditorCode.name, EditorCode)
+    Vue.component(Empty.name, Empty)
+    Vue.component(FieldDate.name, FieldDate)
+    Vue.component(FieldTable.name, FieldTable)
+    Vue.component(FieldText.name, FieldText)
+    Vue.component(FieldTree.name, FieldTree)
+    Vue.component(Img.name, Img)
+    Vue.component(InputNumber.name, InputNumber)
+    Vue.component(ListMenu.name, ListMenu)
+    Vue.component(ListMenuItem.name, ListMenuItem)
+    Vue.component(PowerPage.name, PowerPage)
+    Vue.component(Price.name, Price)
+    Vue.component(Render.name, Render)
+    Vue.component(Search.name, Search)
+    Vue.component(SearchItem.name, SearchItem)
+    Vue.component(Select.name, Select)
+    Vue.component(Splitter.name, Splitter)
+    Vue.component(Table.name, Table)
+    Vue.component(TableColumnFixed.name, TableColumnFixed)
+    Vue.component(TablePagination.name, TablePagination)
+    Vue.component(TableSplitter.name, TableSplitter)
+    Vue.component(TableSummary.name, TableSummary)
+    Vue.component(Thumbnail.name, Thumbnail)
+    Vue.component(Toolbar.name, Toolbar)
+    Vue.component(Uploader.name, Uploader)
+    Vue.component(UploaderQuery.name, UploaderQuery)
+    Vue.component(ValueFormat.name, ValueFormat)
+}
