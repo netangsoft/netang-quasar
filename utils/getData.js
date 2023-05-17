11a71ef787c8b58d81adc46fff681762bed906f7
junmiao <mj287@163.com>
2023-05-18 06:34:37 +0800

    getData

--- utils/getData.js
+++ utils/getData.js
@@ -1,9 +1,11 @@
 import { isRef } from 'vue'
 
+import $n_has from 'lodash/has'
 import $n_isNil from 'lodash/isNil'
 import $n_map from 'lodash/map'
 
 import $n_http from '@netang/utils/http'
+import $n_isValidObject from '@netang/utils/isValidObject'
 
 import $n_config from './config'
 
@@ -19,15 +21,31 @@ export default async function getData(url, pageStatus = null, emptyDescription =
 
     const warn = $n_isNil(pageStatus) || ! isRef(pageStatus)
 
+    // 获取单个请求配置
+    function getSingleHttpOptions(opt) {
+
+        // 如果是对象
+        if ($n_isValidObject(opt)) {
+            opt.url = $n_config('apiDataUrl') + opt.url
+            if (! $n_has(opt, 'warn')) {
+                opt.warn = warn
+            }
+            return opt
+        }
+
+        // 否则为字符串
+        return {
+            url: $n_config('apiDataUrl') + opt,
+            warn,
+        }
+    }
+
     // 如果是数组, 说明需要同时请求多个地址
     // --------------------------------------------------
     if (Array.isArray(url)) {
 
         const result = await $n_http($n_map(url, function (item) {
-            return {
-                url: $n_config('apiDataUrl') + item,
-                warn,
-            }
+            return getSingleHttpOptions(item)
         }))
 
         const res = []
@@ -50,10 +68,7 @@ export default async function getData(url, pageStatus = null, emptyDescription =
 
     // 单个请求
     // --------------------------------------------------
-    const { status, data } = await $n_http({
-        url: $n_config('apiDataUrl') + url,
-        warn,
-    })
+    const { status, data } = await $n_http(getSingleHttpOptions(url))
     if (! status) {
         if (! warn) {
             pageStatus.value = false
